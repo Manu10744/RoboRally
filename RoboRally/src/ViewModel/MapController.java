5c5146c5681bf78046d88c3f5364a5e990b62cb7
brandtner <anna-maria.mww.brandtner@posteo.de>
2019-05-31 15:12:53 +0200

    - Elements for MapView and Controller added

---
+++ RoboRally/src/ViewModel/MapController.java
@@ -0,0 +1,28 @@
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
+
+/**
+ * The class MapController notifies the Server where each robot is positioned and and on which tile(type) it rests
+ */
+public class MapController {
+
+    @FXML
+    private TilePane tilePane;
+
+    @FXML
+    private ImageView robotYellow;
+    @FXML
+    private ImageView robotOrange;
+    @FXML
+    private ImageView robotRed;
+    @FXML
+    private ImageView robotPurple;
+    @FXML
+    private ImageView robotBlue;
+    @FXML
+    private ImageView robotGreen;
+
+
+
+}
