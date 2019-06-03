2d0eb008b8c2b6706357d6cb39b22952db538177
Ivan Dovecar <dovecar@cip.ifi.lmu.de>
2019-06-03 22:42:50 +0200

    - package path fixed - two chat related parameters

--- RoboRally/src/utils/Parameter.java
+++ RoboRally/src/utils/Parameter.java
@@ -1,10 +1,14 @@
-package src.utils;
+package utils;
 
 /**
  * In this class all the Parameters that will occur in the diffenrent classes will show up. <br>
  * The goal is to have them all in one spot.
  *
- * @author Vincent Tafferner
+ * @author Ivan Dovecar
  */
 public class Parameter {
+
+    // Chat
+    public static final String INVALID_CLIENTNAME = "INVALID####";
+    public static final int MAX_NAMESIZE = 15;
 }
