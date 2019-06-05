fabbcde96d86b2053e29126f2063bcb98d11fbd8
Manuel Neumayer <neumayerm@cip.ifi.lmu.de>
2019-06-05 12:34:32 +0200

    StageController doc, binding IDs

--- RoboRally/src/viewmodel/StageController.java
+++ RoboRally/src/viewmodel/StageController.java
@@ -3,28 +3,41 @@ package viewmodel;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
+/**
+ * This class has full control over the primary Stage, which contains all GUI
+ * parts (Chat, Map, Player mat, Opponent mat).
+ *
+ * {@link stage} Wrapper of all GUI Elements.<br>
+ * {@link chatBoardBox} Box containing the Chat and Board (Board contains the map, player
+ * mat and opponent mat).<br>
+ * {@link board} Box containing the map, player mat and opponent mat.<br>
+ * {@link opponentMat} The opponent mat section.<br>
+ * {@link playerMat} The player mat section.<br>
+ * {@link map} The map section.<br>
+ * {@link tilePane} The actual map containing the tiles.<br>
+ * {@link chat} The chat section.<br>
+ *
+ * @author Mia
+ * @author Manuel Neumayer
+ */
 public class StageController {
-
-    //Todo Javadoc
-
     @FXML
-    HBox chatBoardBox;
-
+    private AnchorPane stage;
     @FXML
-    AnchorPane board;
+    private HBox chatBoardBox;
     @FXML
-    VBox boardElements;
+    private VBox board;
     @FXML
-    AnchorPane opponentMat;
+    private AnchorPane opponentMat;
     @FXML
-    AnchorPane map;
+    private AnchorPane playerMat;
     @FXML
-    AnchorPane playerMat;
-
+    private AnchorPane map;
     @FXML
-    AnchorPane chat;
-
-
+    private TilePane tilePane;
+    @FXML
+    private AnchorPane chat;
 }
