8f71667eb28a15d5c089e08d6c1873c7a7ddf2e0
Manuel Neumayer <neumayerm@cip.ifi.lmu.de>
2019-06-05 23:40:23 +0200

    JavaDoc JSON classes, Moving instructions to appropriate place

--- RoboRally/src/modelserver/ServerJSONEncoder.java
+++ RoboRally/src/modelserver/ServerJSONEncoder.java
@@ -2,5 +2,12 @@ package modelserver;
 
 import com.google.gson.*;
 
+/**
+ * This class is responsible for translating Java Objects into their JSON representation in order
+ * to create a JSON message that will be sent to a client.
+ * It makes use of the Gson library.
+ *
+ * @author Manuel Neumayer
+ */
 public class ServerJSONEncoder {
 }
