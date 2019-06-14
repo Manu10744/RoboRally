2222cbc72418201df9a037d85444f2e58023156f
Vincent Tafferner <tafferner@cip.ifi.lmu.de>
2019-06-14 11:03:57 +0200

    extended Parameters - more to follow

--- RoboRally/src/utils/Parameter.java
+++ RoboRally/src/utils/Parameter.java
@@ -17,9 +17,21 @@ public class Parameter {
 
     //Cards
     public static final int SPAM_CARDS_AMOUNT = 36;
-    public static  final int TROJAN_CARDS_AMOUNT = 12;
+    public static final int TROJAN_CARDS_AMOUNT = 12;
     public static final int VIRUS_CARDS_AMOUNT = 18;
     public static final int WORM_CARDS_AMOUNT = 6;
+    public static final int MOVEI_CARDS_AMOUNT = 5;
+    public static final int MOVEII_CARDS_AMOUNT = 3;
+    public static final int MOVEIII_CARDS_AMOUNT = 1;
+    public static final int TURNRIGHT_CARDS_AMOUNT = 3;
+    public static final int TURNLEFT_CARDS_AMOUNT = 3;
+    public static final int AGAIN_CARDS_AMOUNT = 2;
+    public static final int UTURN_CARDS_AMOUNT = 1;
+    public static final int BACKUP_CARDS_AMOUNT = 1;
+    public static final int POWERUP_CARDS_AMOUNT = 1;
+
+    public static final int HAND_CARDS_AMOUNT = 9;
+
 
     //Server
     public static final int SERVER_PORT = 9998;
