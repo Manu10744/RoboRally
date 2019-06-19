fa80ebf4c6e962deb06e15c47fa90f366601c0b6
brandtner <anna-maria.mww.brandtner@posteo.de>
2019-06-19 13:17:36 +0200

    - GameStartedBody added (Mia)

---
+++ RoboRally/src/utils/json/protocol/GameStartedBody.java
@@ -0,0 +1,21 @@
+package utils.json.protocol;
+
+import com.google.gson.annotations.Expose;
+import modelserver.game.Maps.Map;
+
+/** This is the wrapper class for the message body of the 'GameStarted' protocol JSON message. Its main prupose is to sent the map
+ * @author Mia
+ */
+
+public class GameStartedBody {
+    private Map gameMap;
+
+    public GameStartedBody(Map gameMap) {
+        this.gameMap = gameMap;
+    }
+
+
+    public Map getGameMap() {
+        return this.gameMap;
+    }
+}
