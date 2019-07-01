b8c72c7856f7c73ec1a53f10ddfad02b106fc31d
Jessica Gerlach <gerlachj@cip.ifi.lmu.de>
2019-07-01 10:18:49 +0200

    Added chooseRobot view

--- RoboRally/src/viewmodels/StageController.java
+++ RoboRally/src/viewmodels/StageController.java
@@ -1,7 +1,16 @@
 package viewmodels;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import static viewmodels.ChatController.*;
+
+import java.io.IOException;
 
 public class StageController {
 
@@ -28,6 +37,46 @@ public class StageController {
     GridPane playerMat;
     @FXML
     GridPane chat;
+    @FXML
+    ImageView hammerBot;
+    @FXML
+    ImageView hulkX90;
+    @FXML
+    ImageView smashBot;
+    @FXML
+    ImageView twonky;
+    @FXML
+    ImageView spinBot;
+    @FXML
+    ImageView zoomBot;
+
+
+   public void mouseClicked() {
+       hammerBot.setOnMousePressed(event ->{
+           figure.setValue(1);
+           chooseRobot.setVisible(false);
+       });
+       hulkX90.setOnMousePressed(event ->{
+           figure.setValue(2);
+           chooseRobot.setVisible(false);
+       });
+       smashBot.setOnMousePressed(event ->{
+           figure.setValue(3);
+           chooseRobot.setVisible(false);
+       });
+       twonky.setOnMousePressed(event ->{
+           figure.setValue(4);
+           chooseRobot.setVisible(false);
+       });
+       spinBot.setOnMousePressed(event ->{
+           figure.setValue(5);
+           chooseRobot.setVisible(false);
+       });
+       zoomBot.setOnMousePressed(event ->{
+           figure.setValue(6);
+           chooseRobot.setVisible(false);
+       });
+   }
 
 
 }
