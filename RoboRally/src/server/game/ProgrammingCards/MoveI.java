1a4906f647644dba2fb29e583c1e2b902e4f546c
tafferner <v.tafferner@campus.lmu.de>
2019-07-02 15:35:35 +0200

    Added Cards Logic

--- RoboRally/src/server/game/ProgrammingCards/MoveI.java
+++ RoboRally/src/server/game/ProgrammingCards/MoveI.java
@@ -1,5 +1,7 @@
 package server.game.ProgrammingCards;
 
+import static server.game.Robot.*;
+
 /**
  * This class implements the MoveI card.
  *
@@ -12,10 +14,28 @@ public class MoveI extends ProgrammingCard {
     }
 
     /**
-     * This will move the robot one tile in the direction he is facing.
+     * This will move the robot one tile in the direction he is facing. <br>
      */
-    @Override
-    public void activateCard() {
-        //TODO
+    public static void activateCard() {
+        getLineOfSight();
+        getxPosition();  // column
+        getyPosition();  // row
+
+        switch (lineOfSight){
+            case ("up"):
+                xPosition = xPosition -1;
+                break;
+            case ("right"):
+                yPosition = yPosition +1;
+                break;
+            case ("down"):
+                xPosition = xPosition +1;
+                break;
+            case ("left"):
+                yPosition = yPosition -1;
+                break;
+            default:
+                System.out.println("There was a problem with the lineOfSight variable.");
+        }
     }
 }
--- RoboRally/src/server/game/ProgrammingCards/ProgrammingCard.java
+++ RoboRally/src/server/game/ProgrammingCards/ProgrammingCard.java
@@ -13,8 +13,7 @@ public class ProgrammingCard extends Card {
      * This method will be overwritten in the specific programming card classes. <br>
      * Most often it will move the Robot in different ways, always one step at a time.
      */
-    @Override
-    public void activateCard() {
-        //TODO
+    public static void activateCard() {
+        //Please overload
     }
 }
