42fc3b01eb141c05a47e500e74c36710e272d69b
Vincent Tafferner <tafferner@cip.ifi.lmu.de>
2019-07-11 13:36:19 +0200

    fixed burnout and made burnout standard map

--- RoboRally/src/utils/json/MessageDistributer.java
+++ RoboRally/src/utils/json/MessageDistributer.java
@@ -227,7 +227,7 @@ public class MessageDistributer {
         if (numberOfReadyClients >= Parameter.MIN_PLAYERSIZE && numberOfReadyClients == server.getConnectedClients().size()) {
 
             for (Server.ClientWrapper client : server.getConnectedClients()) {
-                Path path = Paths.get("RoboRally/src/resources/maps/dizzyHighway.json");
+                Path path = Paths.get("RoboRally/src/resources/maps/burnout.json");
 
                 try {
                     String map = Files.readString(path, StandardCharsets.UTF_8);
