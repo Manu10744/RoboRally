5e0af35dd996b6597150d3050c45d723cd700ddc
Verena <sadtler@cip.ifi.lmu.de>
2019-07-15 18:14:20 +0200

    startscreenview update

--- RoboRally/src/viewmodels/StartScreenController.java
+++ RoboRally/src/viewmodels/StartScreenController.java
@@ -15,10 +15,15 @@ public class StartScreenController implements Initializable {
     GridPane startScreen;
     @FXML
     ImageView imageView;
+    @FXML
+    ImageView imageView2;
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         imageView.fitWidthProperty().bind(startScreen.widthProperty().divide(Parameter.START_SCREEN_RATIO_WIDTH));
         imageView.fitHeightProperty().bind(startScreen.heightProperty().divide(Parameter.START_SCREEN_RATIO_HEIGHT));
 
+        imageView2.setPreserveRatio(true);
+        imageView2.fitWidthProperty().bind(startScreen.widthProperty().divide(Parameter.START_SCREEN_RATIO_WIDTH));
+        imageView2.fitHeightProperty().bind(startScreen.heightProperty().divide(Parameter.START_SCREEN_RATIO_HEIGHT));
     }
 }
