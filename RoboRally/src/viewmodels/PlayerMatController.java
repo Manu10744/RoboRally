01bfcddf0af6b92c96254ade4953ac3d2c7a83cf
Ivan Dovecar <dovecar@cip.ifi.lmu.de>
2019-07-27 01:57:20 +0200

    Layout update: adjust auto-filled card size to fit perfectly in register, changed lobby background

--- RoboRally/src/viewmodels/PlayerMatController.java
+++ RoboRally/src/viewmodels/PlayerMatController.java
@@ -386,35 +386,35 @@ public class PlayerMatController implements IController {
                 register1.setImage(cardImage);
                 register1.setPreserveRatio(true);
                 register1.fitWidthProperty().bind(playerRegister.widthProperty().divide(Parameter.CARDS_WIDTH));
-                register1.fitHeightProperty().bind(playerRegister.heightProperty());
+                register1.fitHeightProperty().bind(playerRegister.heightProperty().multiply(Parameter.CARDS_REGISTER_SIZE_FACTOR));
                 break;
             }
             case 2: {
                 register2.setImage(cardImage);
                 register2.setPreserveRatio(true);
                 register2.fitWidthProperty().bind(playerRegister.widthProperty().divide(Parameter.CARDS_WIDTH));
-                register2.fitHeightProperty().bind(playerRegister.heightProperty());
+                register2.fitHeightProperty().bind(playerRegister.heightProperty().multiply(Parameter.CARDS_REGISTER_SIZE_FACTOR));
                 break;
             }
             case 3: {
                 register3.setImage(cardImage);
                 register3.setPreserveRatio(true);
                 register3.fitWidthProperty().bind(playerRegister.widthProperty().divide(Parameter.CARDS_WIDTH));
-                register3.fitHeightProperty().bind(playerRegister.heightProperty());
+                register3.fitHeightProperty().bind(playerRegister.heightProperty().multiply(Parameter.CARDS_REGISTER_SIZE_FACTOR));
                 break;
             }
             case 4: {
                 register4.setImage(cardImage);
                 register4.setPreserveRatio(true);
                 register4.fitWidthProperty().bind(playerRegister.widthProperty().divide(Parameter.CARDS_WIDTH));
-                register4.fitHeightProperty().bind(playerRegister.heightProperty());
+                register4.fitHeightProperty().bind(playerRegister.heightProperty().multiply(Parameter.CARDS_REGISTER_SIZE_FACTOR));
                 break;
             }
             case 5: {
                 register5.setImage(cardImage);
                 register5.setPreserveRatio(true);
                 register5.fitWidthProperty().bind(playerRegister.widthProperty().divide(Parameter.CARDS_WIDTH));
-                register5.fitHeightProperty().bind(playerRegister.heightProperty());
+                register5.fitHeightProperty().bind(playerRegister.heightProperty().multiply(Parameter.CARDS_REGISTER_SIZE_FACTOR));
                 break;
             }
         }
